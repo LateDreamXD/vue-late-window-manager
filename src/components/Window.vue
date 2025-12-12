84ad9341d1c3e720d9bab67fdee999dc24377aaf
晚梦 <127290936+LateDreamXD@users.noreply.github.com>
2025-12-12 09:52:15 +0800

    chore(window): close window use minimize animation

--- src/components/Window.vue
+++ src/components/Window.vue
@@ -5,7 +5,7 @@ import type { LateWindowState } from '../types';
 
 const $lwm = inject<LWM.Instance>('$lwm')!;
 
-const props = defineProps<{
+const { window } = defineProps<{
 	window: DeepReadonly<LateWindowState>
 }>();
 
@@ -13,66 +13,79 @@ const dragging = ref(false);
 const offset = ref({ x: 0, y: 0 });
 
 const styleObject = computed(() => ({
-	zIndex: props.window.zIndex,
-	left: `${props.window.position!.x}px`,
-	top: `${props.window.position!.y}px`,
-	width: typeof props.window.size!.width === 'number'? `${props.window.size!.width}px`: props.window.size!.width,
-	height: typeof props.window.size!.height === 'number'? `${props.window.size!.height}px`: props.window.size!.height,
-	display: props.window.isMinimized? 'none': 'flex'
+	zIndex: window.zIndex,
+	left: `${window.position!.x}px`,
+	top: `${window.position!.y}px`,
+	width: typeof window.size!.width === 'number'?
+		   `${window.size!.width}px`: window.size!.width,
+	height: typeof window.size!.height === 'number'?
+			`${window.size!.height}px`: window.size!.height,
+	display: window.isMinimized? 'none': 'flex'
 }));
 
-const isActive = computed(() => props.window.id === $lwm.State.activeWindowId);
+const isActive = computed(() => window.id === $lwm.State.activeWindowId);
 
 const onMouseDown = (e: MouseEvent) => {
-	$lwm.actions.focusWindow(props.window.id);
+	$lwm.actions.focusWindow(window.id);
 }
 
 const startDrag = (e: MouseEvent) => {
-	if(props.window.isMaximized || !props.window.moveable) return;
+	if(window.isMaximized || !window.moveable) return;
 	dragging.value = true;
 	offset.value = {
-		x: e.clientX - props.window.position!.x,
-		y: e.clientY - props.window.position!.y
+		x: e.clientX - window.position!.x,
+		y: e.clientY - window.position!.y
 	}
-	window.addEventListener('mousemove', onDrag);
-	window.addEventListener('mouseup', stopDrag);
+	document.addEventListener('mousemove', onDrag);
+	document.addEventListener('mouseup', stopDrag);
 }
 
 const onDrag = (e: MouseEvent) => {
 	if (!dragging.value) return;
 	let x = e.clientX - offset.value.x;
 	let y = e.clientY - offset.value.y;
-	$lwm.actions.updateWindowPos(props.window.id, { x, y });
+	$lwm.actions.updateWindowPos(window.id, { x, y });
 }
 
 const stopDrag = () => {
 	dragging.value = false;
-	window.removeEventListener('mousemove', onDrag);
-	window.removeEventListener('mouseup', stopDrag);
+	document.removeEventListener('mousemove', onDrag);
+	document.removeEventListener('mouseup', stopDrag);
 }
 
-const maximize = () => $lwm.actions.maximizeWindow(props.window.id);
-const minimize = () => $lwm.actions.minimizeWindow(props.window.id);
-const close = () => $lwm.actions.closeWindow(props.window.id);
+const maximize = () => $lwm.actions.maximizeWindow(window.id);
+const minimize = () => $lwm.actions.minimizeWindow(window.id);
+const close = () => {
+	minimize();
+	setTimeout(() => $lwm.actions.closeWindow(window.id), 300);
+};
 </script>
 
 <template>
-	<div :data-window-id="window.id" :class="{ [$lwm.DefaultOptions.manager!.customClass!.window!]: true, 'focused': isActive, 'minimized': window.isMinimized, 'maximized': window.isMaximized }" :style="styleObject" @mousedown="onMouseDown">
+	<div :data-window-id="window.id" :class="{
+		[$lwm.DefaultOptions.manager!.customClass!.window!]: true, dragging,
+		focused: isActive, minimized: window.isMinimized, maximized: window.isMaximized
+	}" :style="styleObject" @mousedown="onMouseDown">
 		<div class="title-bar" @mousedown="startDrag">
 			<span class="title">
-				<img class="icon" v-if="window.icon" :src="window.icon" alt="props.window.title" />
+				<img class="icon" v-if="window.icon" :src="window.icon" alt="window.title" />
 				{{ window.title }}
 			</span>
 			<div class="controls">
-				<button class="dream-button icon" @click="minimize" title="Minimize" aria-label="Minimize" v-html="icons['window-minimize']"></button>
-				<button class="dream-button icon" @click="maximize" :title="window.isMaximized? 'Restore': 'Maximize'"
-						:aria-label="window.isMaximized? 'Restore': 'Maximize'" v-html="window.isMaximized? icons.compress: icons.expand"
-				></button>
-				<button class="dream-button danger icon" @click="close" title="Close" aria-label="Close" v-html="icons.xmark"></button>
+				<button class="dream-button icon" @click="minimize" title="Minimize"
+						aria-label="Minimize" v-html="icons['window-minimize']"></button>
+				<button class="dream-button icon" @click="maximize"
+						:title="window.isMaximized? 'Restore': 'Maximize'"
+						:aria-label="window.isMaximized? 'Restore': 'Maximize'"
+						v-html="window.isMaximized? icons.compress: icons.expand"></button>
+				<button class="dream-button danger icon" @click="close" title="Close"
+						aria-label="Close" v-html="icons.xmark"></button>
 			</div>
 		</div>
 		<div class="workspace">
-			<slot></slot>
+			<div class="content">
+				<slot></slot>
+			</div>
 		</div>
 	</div>
 </template>
